c238b42ce53cea51ec66676d6a83541dc013f2cd
2crsg4 <aniko.sasa14@gmail.com>
2021-08-05 15:45:47 +0900

    [Modify]自動生成されるファイルを制限、タイムゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,16 @@ module GoOut
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.generators.template_engine = :slim
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
