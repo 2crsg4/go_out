7b882d7f1c80a4c83e68257973bbc90777beb99a
2crsg4 <aniko.sasa14@gmail.com>
2021-08-09 21:56:48 +0900

    [Add]sorcery_core users table

---
+++ db/migrate/20210805064645_sorcery_core.rb
@@ -0,0 +1,15 @@
+class SorceryCore < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :user_name,         null: false
+      t.string :nickname
+      t.integer :role, default: 0, null: false
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps                 null: false
+    end
+
+    add_index :users, :user_name, unique: true
+  end
+end
